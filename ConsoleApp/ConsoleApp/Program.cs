9ac45439f3c6f539aca7a1dadf88bdbda42d23b9
MarcinMCH <marcinchmara93@gmail.com>
2023-03-05 22:01:28 +0100

    lekcja 4

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, Marcin!");
+﻿var number1 = 50;
+var number2 = 10;
+
+// ==
+// !=
+// >
+// <
+// >=
+// <=
+
+var name = "Marcin";
+var age = 29;
+
+if (name == "Marcin" || age < 40)
+{
+    Console.WriteLine("JESTEM MARCINEM LUB MAM PONIŻEJ 40 LAT.");
+}
+else
+{
+    Console.WriteLine("JESTEM KIMŚ INNYM");
+}
+
+// &&
+// ||
+// !
