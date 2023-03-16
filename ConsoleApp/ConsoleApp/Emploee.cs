a1fde2009c54f88f6b87660b95e9e3da0b48331a
MarcinMCH <marcinchmara93@gmail.com>
2023-03-16 23:22:14 +0100

    Praca domowa dzień 6

---
+++ ConsoleApp/ConsoleApp/Emploee.cs
@@ -0,0 +1,41 @@
+﻿namespace ChallangeApp;
+public class Emploee
+{
+    // Konstruktor
+    public Emploee(string name, string surname, int age)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    // Pola
+    public string name { get; private set; }
+    public string surname { get; private set; }
+    public int age { get; private set; }
+
+
+    // Pola do metody maxScoreMetod
+    private List<int> score = new List<int>();
+    private int point;
+    public int max;
+
+
+    //Metody
+    public void AddScore(int score)
+    {
+        this.score.Add(score);
+    }
+
+    public void maxScoreMetod()
+    {
+        this.point = 0;
+        foreach (int score in score)
+        {
+            if (score > this.point)
+            {
+                this.point = score;
+            }
+            this.max = this.point;
+        }
+    }
+}
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,11 +1,52 @@
 ﻿using ChallangeApp;
 
-User user1 = new User("Marcin", "Pop2344qa");
-User user2 = new User("Asia", "KokoSpoko0");
-User user3 = new User("Kacper", "13254");
-User user4 = new User("Marta", "KLopsik");
+Emploee emploee1 = new Emploee("Karol", "Kowalski", 26);
+Emploee emploee2 = new Emploee("Monika", "Nowak", 18);
+Emploee emploee3 = new Emploee("Hans", "Klos", 80);
 
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
+emploee1.AddScore(6);
+emploee1.AddScore(1);
+emploee1.AddScore(3);
+emploee1.AddScore(9);
+emploee1.AddScore(3);
+
+emploee2.AddScore(4);
+emploee2.AddScore(8);
+emploee2.AddScore(8);
+emploee2.AddScore(7);
+emploee2.AddScore(9);
+
+emploee3.AddScore(1);
+emploee3.AddScore(1);
+emploee3.AddScore(5);
+emploee3.AddScore(2);
+emploee3.AddScore(1);
+
+//aktualnie nie wiem jak zrobić metodę która zwraca wartość
+
+emploee1.maxScoreMetod();
+emploee2.maxScoreMetod();
+emploee3.maxScoreMetod();
+
+List<(int, Emploee)> check = new List<(int, Emploee)>
+{
+    (emploee1.max, emploee1),
+    (emploee2.max, emploee2),
+    (emploee3.max, emploee3)
+};
+
+var maxValue = 0;
+var emploeeWithHightScore = emploee1;
+foreach (var (a, b) in check)
+{
+    if (a > maxValue)
+    {
+        maxValue = a;
+        emploeeWithHightScore = b;
+    }
+}
+
+Console.WriteLine("Procownik z najwyższą oceną (" + maxValue + ") to:");
+Console.WriteLine("Imię: " + emploeeWithHightScore.name);
+Console.WriteLine("Nazwisko: " + emploeeWithHightScore.surname);
+Console.WriteLine("Wiek: " + emploeeWithHightScore.age);
